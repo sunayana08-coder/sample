aa5adce11c02df58103dcde1f40d78af04fa8c9e
sunayana08-coder <sunayana.shaik@outlook.com>
2024-09-03 18:09:57 +0530

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,12 @@
 resource "local_file" "My-pet"{
     filename = "pets.txt"
-    content = "My pet is a cat"
+    content = "my cat name is ${random_pet.petname.id}"
 }
 resource "random_pet" "petname"{
     prefix = "Miss"
     separator = "."
     length = "1"
+}
+output "pet_name" {
+    value = random_pet.petname.id
 }
